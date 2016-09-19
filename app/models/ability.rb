f5dc8b29b4cf8d29c6a43149351c91e50e3fe27a
Marco Vinicius Soares Dalalba <vinisdl@hotmail.com>
2016-09-19 00:42:32 +0000

    fiaxing roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,15 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.has_role?(:metadmin)
+    if user.has_role?(:admin)
       can :manage, :all
     elsif user.has_role?(:editor)
       can :access, :rails_admin
       can :dashboard
+      can :read, :all
+    else if user.has_role?(:user)
+      can :read, :all
     end
-
-
     # cannot :export, :all
     # cannot :create, [Carousel, Location]
     # cannot :edit, Location
